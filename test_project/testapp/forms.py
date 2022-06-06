36c57df24b4d3e35731055f70cb0879e5b77c130
Dmenis <dmenisdev@gmail.com>
2022-06-06 21:17:15 +0700

    Experimenting with the test_project
    Experimenting with the test_project

--- test_project/testapp/forms.py
+++ test_project/testapp/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+
 try:
-    from django.core.urlresolvers import reverse_lazy
+	from django.core.urlresolvers import reverse_lazy
 except ImportError:
-    from django.urls import reverse_lazy
+	from django.urls import reverse_lazy
 
 from django_addanother.widgets import AddAnotherWidgetWrapper, AddAnotherEditSelectedWidgetWrapper
 
@@ -10,21 +11,25 @@ from .models import Player
 
 
 class PlayerForm(forms.ModelForm):
-    class Meta:
-        model = Player
-        fields = ['name', 'current_team', 'future_team', 'previous_teams']
-        widgets = {
-            'current_team': AddAnotherWidgetWrapper(
-                forms.Select(),
-                reverse_lazy('add_team'),
-            ),
-            'future_team': AddAnotherEditSelectedWidgetWrapper(
-                forms.Select,
-                reverse_lazy('add_team'),
-                reverse_lazy('edit_team',args=['__fk__']),
-            ),
-            'previous_teams': AddAnotherWidgetWrapper(
-                forms.SelectMultiple,
-                reverse_lazy('add_team'),
-            )
-        }
+	def __init__(self, *args, **kwargs):
+		super(PlayerForm, self).__init__(*args, **kwargs)
+		self.fields['current_team'].widget.attrs['hidden'] = True
+
+	class Meta:
+		model = Player
+		fields = ['name', 'current_team', 'future_team', 'previous_teams']
+		widgets = {
+			'current_team': AddAnotherWidgetWrapper(
+				forms.Select(),
+				reverse_lazy('add_team'),
+			),
+			'future_team': AddAnotherEditSelectedWidgetWrapper(
+				forms.Select,
+				reverse_lazy('add_team'),
+				reverse_lazy('edit_team', args=['__fk__']),
+			),
+			'previous_teams': AddAnotherWidgetWrapper(
+				forms.SelectMultiple,
+				reverse_lazy('add_team'),
+			)
+		}
